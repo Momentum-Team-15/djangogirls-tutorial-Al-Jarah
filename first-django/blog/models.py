eac82046554481be792bf852d9e8430e0b740863
Elijah Brown <elibrown0731@gmail.com>
2022-10-19 10:48:42 -0400

    added a comments for the Post Model

--- first-django/blog/models.py
+++ first-django/blog/models.py
@@ -4,9 +4,13 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    # this is a link to another model.
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # this is how you define text with a limited number of characters.
     title = models.CharField(max_length=200)
+    # this is for long text without a limit. Sounds ideal for blog post content, right?
     text = models.TextField()
+    # this is a date and time.
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
