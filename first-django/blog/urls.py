3726fc5e0349eb596aa794197653951e44e1015b
Elijah Brown <elibrown0731@gmail.com>
2022-10-20 10:44:02 -0400

    added a post form

--- first-django/blog/urls.py
+++ first-django/blog/urls.py
@@ -8,9 +8,15 @@ the right place to go if someone enters your website at the 'http://127.0.0.1:80
 
 from django.urls import path
 from . import views
+from .forms import PostForm
 
 urlpatterns = [
     #this is the URL pattern that will be used to show the post list
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
