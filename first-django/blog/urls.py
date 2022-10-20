12db868e4496424d2df5d086cfcec4cccb298ef3
Elijah Brown <elibrown0731@gmail.com>
2022-10-20 11:03:12 -0400

    added commment indent

--- first-django/blog/urls.py
+++ first-django/blog/urls.py
@@ -10,7 +10,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #this is the URL pattern that will be used to show the post list
+    # this is the URL pattern that will be used to show the post list
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
